52975f2afc39486dd5bf1b7fade73853dbee480d
diegomzmn <diego_mzmn@hotmail.com>
2021-01-04 15:37:59 +0100

    Spawn transform improved

--- Source/ComoPicaro/MapGeneration/TileLevelScriptActor.cpp
+++ Source/ComoPicaro/MapGeneration/TileLevelScriptActor.cpp
@@ -5,7 +5,25 @@
 
 AEnemy* ATileLevelScriptActor::SpawnEnemy(TSubclassOf<AEnemy> enemyClass)
 {
-	AActor* enemyActor = GetWorld()->SpawnActorAbsolute(enemyClass, SpawnTargetPoint->GetActorTransform());
+	FTransform spawnTransform = SpawnTargetPoint->GetTransform();
+
+	AEnemy* defaultEnemy = static_cast<AEnemy*>(enemyClass->GetDefaultObject());
+	FVector defaultScale = defaultEnemy->GetActorScale();
+	spawnTransform.SetScale3D(defaultScale);
+
+	FActorSpawnParameters spawnParams;
+	spawnParams.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AdjustIfPossibleButAlwaysSpawn;
+
+	AActor* enemyActor = GetWorld()->SpawnActorAbsolute(enemyClass, spawnTransform, spawnParams);
 	AEnemy* enemy = static_cast<AEnemy*>(enemyActor);
+
+	FVector originalLocation = enemy->GetActorLocation();
+	FVector boundOrigin;
+	FVector boundExtent;
+	enemy->GetActorBounds(false, boundOrigin, boundExtent);
+	float enemyHeight = boundExtent.Z - boundOrigin.Z;
+	originalLocation.Z += enemyHeight;
+	enemy->SetActorLocation(originalLocation);
+
 	return enemy;
 }
