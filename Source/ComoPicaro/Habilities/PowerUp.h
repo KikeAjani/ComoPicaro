0fe87fe8174efa222e253e9d3b98944e8a16cc43
KikeAjani <kikevalps3@gmail.com>
2021-01-10 19:28:52 +0100

    Event when PowerUp picked

--- Source/ComoPicaro/Habilities/PowerUp.h
+++ Source/ComoPicaro/Habilities/PowerUp.h
@@ -1,9 +1,13 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
+#include "Components/BoxComponent.h"
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
 #include "PowerUp.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FPowerUpPickedDelegate);
+
 
 UCLASS(BlueprintType)
 class COMOPICARO_API APowerUp : public AActor 
@@ -14,11 +18,14 @@ public:
 	// Sets default values for this actor's properties
 	APowerUp();
 
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
-		class UStaticMeshComponent* StaticMeshPowerUp;
-
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
-		class UBoxComponent* BoxComponent;
+	UPROPERTY(BlueprintAssignable)
+		FPowerUpPickedDelegate PowerUpPickedDelegate;
+// 
+// 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+// 		class UStaticMeshComponent* StaticMeshPowerUp;
+// 
+// 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+// 		class UBoxComponent* BoxComponent;
 
 protected:
 	// Called when the game starts or when spawned
@@ -28,5 +35,9 @@ public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
-
+	UFUNCTION(BlueprintCallable)
+	void DelegateBroadcast();
+	
+// 	UFUNCTION()
+// 	void OnBoxBeginOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
 };
